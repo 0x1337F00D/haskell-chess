6e2ee05b70a0d3e9052a1fc32adc75924c12a189
Paul <97072789+0x1337F00D@users.noreply.github.com>
2025-07-09 22:42:10 +0200

    Complete core data types

--- test/Main.hs
+++ test/Main.hs
@@ -2,6 +2,7 @@ module Main (main) where
 
 import Test.Hspec
 import Chess.Types
+import Chess.Bitboard
 
 main :: IO ()
 main = hspec $ do
@@ -20,4 +21,14 @@ main = hspec $ do
       fmap show (parseSquare "a1") `shouldBe` Just "a1"
     it "parses invalid" $
       parseSquare "i9" `shouldBe` Nothing
+
+  describe "bitboard basics" $ do
+    it "lsb of BB_E2 is square 12" $
+      lsb BB_E2 `shouldBe` Just 12
+    it "popcount of a file is 8" $
+      popcount bbFileA `shouldBe` 8
+    it "flipVertical A1 is A8" $
+      flipVertical BB_A1 `shouldBe` BB_A8
+    it "shiftUp from A2 gives A3" $
+      shiftUp BB_A2 `shouldBe` BB_A3
 
